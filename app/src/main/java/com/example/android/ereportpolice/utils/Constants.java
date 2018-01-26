8c8d512191f105750fb59a20f468f976a7a6f540
Kenny-Rogers <ylodonu@gmail.com>
2018-01-26 15:56:37 +0000

    file to hold all the constants of the project
    Signed-off-by: Kenny-Rogers <ylodonu@gmail.com>

---
+++ app/src/main/java/com/example/android/ereportpolice/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.example.android.ereportpolice.utils;
+
+/**
+ * Created by krogers on 1/26/18.
+ */
+
+public class Constants {
+    //holds the IP ADDRESS or URL of the server
+    public static final String SERVER_URL = "https://192.168.122.1/";
+}
